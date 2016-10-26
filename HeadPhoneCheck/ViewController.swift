f12b8e9364cb4df61e979dda1ad52ff86df49e2b
ShotaTakai <takaishota@gmail.com>
2016-10-26 19:31:05 +0900

    first commit

--- HeadPhoneCheck/ViewController.swift
+++ HeadPhoneCheck/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func clickCheckButton(_ sender: UIButton) {
+        if headPhoneArePlugged() {
+            result.text = "イヤホンがささってます"
+        } else {
+            result.text = "イヤホンはささってません"
+        }
+    }
 
-
+    
+    private func headPhoneArePlugged() -> Bool {
+        let session = AVAudioSession.sharedInstance()
+        var plugged = false
+        
+        let routeDesc: AVAudioSessionRouteDescription = session.currentRoute
+        
+        for desc:AVAudioSessionPortDescription in routeDesc.outputs {
+            let type = desc.portType
+            
+            if type == AVAudioSessionPortHeadphones {
+                plugged = true
+            }
+        }
+        return plugged
+        
+    }
 }
 
